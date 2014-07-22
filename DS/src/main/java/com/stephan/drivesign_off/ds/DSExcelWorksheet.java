3777730433715de612f224366d3afe026822b7dc
John Visser <j.j.visser@hhs.nl>
2014-07-22 17:15:53 +0200

    Comment toegevoegd [Scrumwise, Task #15]

--- DS/src/main/java/com/stephan/drivesign_off/ds/DSExcelWorksheet.java
+++ DS/src/main/java/com/stephan/drivesign_off/ds/DSExcelWorksheet.java
@@ -73,6 +73,7 @@ public class DSExcelWorksheet {
         int currentIndex = 0;
 
         // process rows
+        //comment john
         while (worksheetRowIterator.hasNext()) {
 
             // get the row to be processed
